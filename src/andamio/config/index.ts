01a5249ef45c278bc7243ff33ec752064dd7af78
jamesdunseith <jdunseith@gimbalabs.io>
2023-11-29 13:29:53 -0500

    Deploy Dework PBL on Preprod

--- src/andamio/config/index.ts
+++ src/andamio/config/index.ts
@@ -1,23 +1,23 @@
 import { AndamioConfig, CourseManagementConfig, ProjectManagementConfig } from "@andamiojs/core";
 
 // Project Management
-import { contributorMinter, contributorReference } from "./contributor";
-import { escrows } from "./escrow";
-import { treasury } from "./treasury";
+// import { escrows } from "./escrow";
+
+
 
 // Course Management
-import { assignment } from "./assignment";
-import { courseReference } from "./courseReference";
-import { learnerReference } from "./learnerReference";
-import { learnerMintingReference } from "./learnerMintingReference";
-import { moduleMintingReference } from "./moduleMintingReference";
+import assignment from "../cardano/plutus/assignment.json"
+import courseReference from "../cardano/plutus/courseReference.json"
+import learnerReference from "../cardano/plutus/learnerReference.json"
+import learnerMintingReference from "../cardano/plutus/learnerMintingReference.json"
+import moduleMintingReference from "../cardano/plutus/moduleMintingReference.json"
 
 // Tokens
 import courseManagementTokens from './courseManagementTokens.json'
 import projectManagementTokens from './projectManagementTokens.json'
 
-export const andamioConfig: AndamioConfig<CourseManagementConfig & ProjectManagementConfig> = {
-  title: "Dework PBL",
+export const andamioConfig: AndamioConfig<CourseManagementConfig> = {
+  title: "Andamio PBL",
   baseAddress: "",
   enterpriseAddress: "",
   rewardAddress: "",
@@ -33,16 +33,11 @@ export const andamioConfig: AndamioConfig<CourseManagementConfig & ProjectManage
     moduleMintingReference: moduleMintingReference,
     courseManagementTokens: courseManagementTokens,
     coursemanagementNftArtwork: {
-      // move to a file?
-      learnerNFTURL: "https://www.andamio.io/andamio.png",
-      courseModuleNFTURL: "https://www.andamio.io/andamio.png",
-      courseCreatorNFTURL: "https://www.andamio.io/andamio.png",
-      courseDeciderNFTURL: "https://www.andamio.io/andamio.png",
+      // Update Me!
+      learnerNFTURL: "ipfs://bafkreidqhvdxgcgd2lq4zihg376bpbwc7di655vc34ck6ex3qggdnomscy",
+      courseModuleNFTURL: "ipfs://www.andamio.io/andamio.png",
+      courseCreatorNFTURL: "ipfs://bafkreia5krw4q4tx4whihrf5byvtfgjuai45s6v63vl6mlizhpd4j62yiu",
+      courseDeciderNFTURL: "ipfs://bafkreicyijhoyivlfhcxnnnqs2cmzz5wgqukg5fwuq3z6za6kiab2pbsia",
     },
-    contributorMinter: contributorMinter,
-    contributorReference: contributorReference,
-    escrows: escrows,
-    treasury: treasury,
-    projectManagementTokens: projectManagementTokens,
   },
 };
